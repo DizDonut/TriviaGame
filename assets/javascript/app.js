8eb73dd7e73add837108a2bef75be60e07d024b3
Michael R. Bechtel <mikebec20@gmail.com>
2017-06-29 19:41:51 -0400

    js updates to functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,85 +1,98 @@
 var game = {
   questionsArr: [{
     question: "What vehicle was driven by Caractacus Potts?",
-    answer: "Chitty Chitty Bang Bang",
+    Answers: ["Chitty Chitty Bang Bang","Mystery Machine","Anglia","Ecto-1"],
   }, {
     question: "Which rock start plays the role of vampire in the 1983 movie 'The Hunger'?",
-    answer: "David Bowie",
+    Answers: ["David Bowie","Billy Idol","Gene Simmons","Ozzie Osbourne"],
   },{
     question: "What 1990 hit song by Jane's Addiction begins with the sound of dogs barking?",
-    answer: "Been Caught Stealing",
+    Answers: ["Been Caught Stealing","Jane Says","Ocean Size","Just Because"],
   },{
     question: "In the TV show 'The Simpsons' what is the name of the Springfield's music store?",
-    answer: "King Toot's",
+    Answers: ["King Toot's","Moe's","Kwik-E-Mart","Lard Lad Donuts"],
   },{
     question: "What is singer Meat Loafs real name?",
-    answer: "Michael Lee Aday",
+    Answers: ["Michael Lee Aday","Prince Rogers Nelson","Georgios Kyriacos Panayiotou","Richard Bachman"],
   },{
     question: "In the 1988 move, 'Die Hard', what does John McClane leave in the limo?",
-    answer: "A teddy bear",
+    Answers: ["A teddy bear","His lighter","A TV Dinner","A walkie talkie"],
   },{
     question: "What is the name of the prince in Disney's 'Sleeping Beauty'?",
-    answer: "Phillip",
+    Answers: ["Phillip","Florian","Henry","Eric"],
   },{
     question: "Whose adventures on TV take place on the fictional island of Sodor?",
-    answer: "Thomas the Tank Engine",
+    Answers: ["Thomas the Tank Engine","Goku","Finn & Jake","Sayid Jarrah"],
   },{
     question: "For what does the 'T' stand in the name of Star Trek's captain James T. Kirk?",
-    answer: "Tiberius",
+    Answers: ["Tiberius","Thomas","Tranquility","Terrance"],
   },{
     question: "In what movie is $200,000 of Confederate gold buried in the grave marked 'Unknown' next to Arch Stanton?",
-    answer: "The Good, The Bad, & The Ugly",
+    Answers: ["The Good, The Bad, & The Ugly","True Grit","No Country for Old Men","A Fistful of Dollars"],
   },{
     question: "What Madonna video won the 1998 MTV Video of the Year Award?",
-    answer: "Ray of Light",
+    Answers: ["Ray of Light","Like a Virgin","Hung Up","Nothing Really Matters"],
   },{
     question: "What actor died at the age of 24 while driving his Porsche Spyder?",
-    answer: "James Dean",
+    Answers: ["James Dean","Paul Walker","Bruce Lee","Brandon Lee"],
   },{
     question: "What Broadway Musical is based off the opera 'La Bohen'?",
-    answer: "Rent",
+    Answers: ["Rent","Wicked","My Fair Lady","Les Miserables"],
   },{
     question: "What move featured the fictional band 'The Soggy Bottom Boys'?",
-    answer: "O Brother, Where Art Thou",
+    Answers: ["O Brother, Where Art Thou","The Great Outdoors","Get On Up","That Thing You Do!"],
   },{
     question: "What year did 'The Tonight Show' first air?",
-    answer: "1954",
+    Answers: ["1954","1955","1956","1957"],
   },{
     question: "In which US state was rapper 'Eminem' born?",
-    answer: "Missouri",
+    Answers: ["Missouri","Michigan","Indiana","Ohio"],
   },{
     question: "What was Walt Disney's middle name?",
-    answer: "Elias",
+    Answers: ["Elias","Tobias","Ray","Thomas"],
   },{
     question: "For what movie did Dan Aykroyd receive an Oscar nomination?",
-    answer: "Driving Miss Daisy",
+    Answers: ["Driving Miss Daisy","Ghostbusters","Blues Brothers","Spies Like Us"],
   },{
     question: "Singer Stefani Joanne Angelina Germanotta is better known by what stage name?",
-    answer: "Lady Gaga",
+    Answers: ["Lady Gaga","Gwen Stefani","Pink","Eve"],
   },{
     question: "What movie did the villain Norman Bates appear in?",
-    answer: "Pyscho",
+    Answers: ["Pyscho","Saw","One Flew Over the Cuckoos Nest","Birds"],
   },
 ],
 
   maxQuestions: 10,
   buttonDisabled: false,
   questionIncrement: 0,
-  optionsArr: [],
-  startingScore: 100,
+  questionsAskedArr: [],
+  startingScore: 1000,
   accumScore: 0,
   correctAnswers: 0,
   incorrectAnswers: 0,
   notAnswered: 0,
 
-  randomQuestion: function(){
-    var random = this.questionsArr[Math.floor(Math.random() * this.questionsArr.length)].question;
-    console.log(random);
-    $("#question").html(random);
-    this.buttonDisabled = true;
-    if(this.buttonDisabled){
-      $("#question").prop("disabled", true);
+  randomNoRepeats: function(){
+    var index = Math.floor(Math.random() * this.questionsArr.length);
+    console.log(index);
+    var item = this.questionsArr[index];
+    console.log(item);
+    var removedItem = this.questionsArr.splice(index, 1);
+    this.questionsAskedArr.push(removedItem);
+
+    if(this.questionsAskedArr.length === this.maxQuestions){
+      alert("Game Over!");
     }
+    return removedItem;
+  },
+
+  displayQuestion: function(arg1){
+    var ques = arg1.question;
+    return ques;
+  },
+
+  displayAnswers: function(arg2){
+    var answ = arg2.correctAnswers[0];
   },
 
 };
@@ -87,7 +100,13 @@ var game = {
 $("document").ready(function(){
 
   $("#go").on("click", function(){
-    game.randomQuestion();
+    $(".progress-bar").animate({
+      width: "0%"
+    }, 10 * 1000);
+      random = game.randomNoRepeats();
+
+      for()
+
   });
 
 });
